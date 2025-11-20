f3e639c4e4964e7e67aeb6d75db7b0c1cbbf095a
tito10047 <jozef@mostka.sk>
2025-11-20 10:51:38 +0100

    wip

---
+++ tests/App/AssetMapper/Src/Controller/IndexAction.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tito10047\BatchSelectionBundle\Tests\App\AssetMapper\Src\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+class IndexAction extends AbstractController{
+
+
+    #[Route('/', name: 'index')]
+    public function __invoke()
+    {
+
+        return $this->render('index.html.twig',[
+        ]);
+    }
+
+}
---
+++ tests/App/AssetMapper/Src/Factory/RecordIntegerFactory.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tito10047\BatchSelectionBundle\Tests\App\AssetMapper\Src\Factory;
+
+use Doctrine\ORM\EntityRepository;
+use Tito10047\BatchSelectionBundle\Tests\App\AssetMapper\Src\Entity\RecordInteger;
+use Zenstruck\Foundry\Persistence\PersistentProxyObjectFactory;
+use Zenstruck\Foundry\Persistence\Proxy;
+use Zenstruck\Foundry\Persistence\ProxyRepositoryDecorator;
+
+/**
+ * @extends PersistentProxyObjectFactory<RecordInteger>
+ */
+final class RecordIntegerFactory extends PersistentProxyObjectFactory{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    public static function class(): string
+    {
+        return RecordInteger::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    protected function defaults(): array|callable
+    {
+        return [
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(RecordInteger $recordInteger): void {})
+        ;
+    }
+}
